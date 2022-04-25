3dcfacf365905af9d22d1164244deb3743e42ae7
Advorix <whatever1692@gmail.com>
2022-04-25 14:18:40 -0500

    Wrap code into a function

--- Exam 6/script.js
+++ Exam 6/script.js
@@ -1,24 +1,35 @@
 /*
-  Days in year: 366
-  Probability = 1 - Pn
-  Formula: Probability = 1 - 367 - n/366*/
+ Outputs are at the bottom.   
+ Days in year: 366
+ Probability = 1 - Pn
+ Formula: Probability = 1 - ...  * 367 - n/366
+ */
 
-  currentProbability = 0;
-  probabilityHelper = 365/366; //this is to keep track of the 365/366 * 364/366 *...
-  dayCount = 364; //remove first two for ease of setting up the math
-  threshold = 0.5;
-  thresholdCheck = false;
-  peopleCount = 2; //because 366/366 and 365/366
-  while (!thresholdCheck) {
-      if (currentProbability >= threshold) {
-          thresholdCheck = true;
-      }
-      else {
-          peopleCount++;
-          currentProbability = 1 - dayCount/366 * probabilityHelper;
-          probabilityHelper *= dayCount/366;
-          dayCount--;
-      }
+  
+ function birthdayProblem(desiredProbability) {
+
+     currentProbability = 0;
+     probabilityHelper = 365/366; //this is to keep track of the 365/366 * 364/366 *...
+     dayCount = 364; //remove first two for ease of setting up the math
+     threshold = desiredProbability;
+     thresholdCheck = false;
+     peopleCount = 2; //because 366/366 and 365/366
+
+     while (!thresholdCheck) {
+       if (currentProbability >= threshold) {
+           thresholdCheck = true;
+       }
+       else {
+           peopleCount++;
+           currentProbability = 1 - dayCount/366 * probabilityHelper;
+           probabilityHelper *= dayCount/366;
+           dayCount--;
+        }
+     }
+
+     console.log("Minimum people in the room: " + peopleCount);
+     console.log("Probability: " + currentProbability);
   }
-  console.log("Minimum people in the room: " + peopleCount);
-  console.log("Probability: " + currentProbability);
+
+birthdayProblem(0.5)
+
